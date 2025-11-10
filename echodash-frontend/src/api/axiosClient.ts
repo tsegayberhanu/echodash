155acb5dfc061d975713d25ab97dc1127b39d133
tsegay <tsegay.berhanu186@gmail.com>
2025-11-10 16:42:54 +0300

    chore(env): configuring env variable for frontend

--- echodash-frontend/src/api/axiosClient.ts
+++ echodash-frontend/src/api/axiosClient.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API_BASE_URL = import.meta.env.API_BASE_URL as string
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string
 const axiosClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
