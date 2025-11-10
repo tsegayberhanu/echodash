f3c72cf28ebec0764139d20fc4f96fcf5b8ea2cc
tsegay <tsegay.berhanu186@gmail.com>
2025-11-10 14:00:34 +0300

    fix: cors / issue

--- echodash-backend/src/config/cors.config.ts
+++ echodash-backend/src/config/cors.config.ts
@@ -1,4 +1,4 @@
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', "http://localhost:4173/", "https://echodash-eight.vercel.app/"];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', "http://localhost:4173/", "https://echodash-eight.vercel.app"];
 export const corsOptions = {
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
