20540545df92cae6ab0bbe48493e78b8bebb8977
tsegay <tsegay.berhanu186@gmail.com>
2025-11-10 13:03:32 +0300

    mongoose connection

--- echodash-backend/src/server.ts
+++ echodash-backend/src/server.ts
@@ -1,12 +1,18 @@
 import "dotenv/config";
 import app from "./app.js";
-import connectDB from "./config/db.config.js";
+import mongoose from "mongoose";
 const PORT = process.env.PORT || 3000;
-const MONGO_URI= process.env.MONGO_URI as string
+const MONGO_URI = process.env.MONGO_URI as string;
 
 if (!MONGO_URI) {
   throw new Error("MONGO_URI is not defined in environment variables");
 }
 
-await connectDB(MONGO_URI);
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+ mongoose.connect(MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err: any) => {
+    console.log("mongodb connection error", err);
+    process.exit(1);
+  });
